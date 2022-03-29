e578890cf574560bad3ab39924ca9332f30531f7
Dionisio Benevides <dbnvides@gmail.com>
2022-03-29 14:56:25 -0300

    Commit do site completo

---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {   
+        productName: 'Foldable Mini Drone',
+        productNumber: '67896',
+        paymentStatus: 'Due',
+        shipping: 'Pedding'
+    },
+    {   
+        productName: 'Foldable Mini Drone',
+        productNumber: '67896',
+        paymentStatus: 'Due',
+        shipping: 'Declined'
+    },
+    {   
+        productName: 'Foldable Mini Drone',
+        productNumber: '67896',
+        paymentStatus: 'Due',
+        shipping: 'Delivered'
+    },
+    {   
+        productName: 'Foldable Mini Drone',
+        productNumber: '67896',
+        paymentStatus: 'Due',
+        shipping: 'Pedding'
+    },
+    {   
+        productName: 'Foldable Mini Drone',
+        productNumber: '67896',
+        paymentStatus: 'Due',
+        shipping: 'Pedding'
+    },
+    {   
+        productName: 'Foldable Mini Drone',
+        productNumber: '67896',
+        paymentStatus: 'Due',
+        shipping: 'Pedding'
+    },
+    {   
+        productName: 'Foldable Mini Drone',
+        productNumber: '67896',
+        paymentStatus: 'Due',
+        shipping: 'Pedding'
+    },
+]
